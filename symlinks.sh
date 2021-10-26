47649fed106de1b6ff70a85c1c03203344bfaa89
Ryan Cheevers-Brown <ryan@cheevtech.com>
2021-10-26 03:05:33 -0400

    updating symlink script

--- symlinks.sh
+++ symlinks.sh
@@ -3,6 +3,12 @@
 
 DIR=$(dirname "$0")
 
+if test "$#" -eq 0; then
+	echo "Usage is symlinks.sh <app1> <app2> <app3>, etc etc."
+	exit 1
+fi
+
+
 if [ ! -d "$DIR/symlinks" ]; then
 	mkdir "$DIR/symlinks"
 fi
